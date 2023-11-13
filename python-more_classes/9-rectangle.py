120ff3e6847c5b11271b3452150a33352d56de06
Mkellia <k.muzira@gmail.com>
2023-11-13 10:57:52 -0800

    updated

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -24,8 +24,6 @@ class Rectangle:
     
     print_symbol = "#"
     
-
-    
     def __init__(self, width=0, height=0):
         
         """Initialize a new Rectangle.
@@ -37,45 +35,39 @@ class Rectangle:
             height (int): The height of the new rectangle.
 
         """
-        
-        type(self).number_of_instances += 1
-        
-        self.width = width
-        
-        self.height = height
-        
 
-        
-    @property
+        type(self).number_of_instances += 1
+
+        self.width = width
+
+        self.height = height
     
+    @property
+
     def width(self):
-        
+
         """Get/set the width of the Rectangle."""
-        
+
         return self.__width
     
-
-    
     @width.setter
-    
-    def width(self, value):
-        
-        if not isinstance(value, int):
-            
-            raise TypeError("width must be an integer")
-        
-        if value < 0:
-            
-            raise ValueError("width must be >= 0")
-        
-        self.__width = value
-        
 
-        
-    @property
+    def width(self, value):
+
+        if not isinstance(value, int):
+
+            raise TypeError("width must be an integer")
+
+        if value < 0:
+
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
     
+    @property
+
     def height(self):
-        
+
         """Get/set the height of the Rectangle."""
         
         return self.__height
