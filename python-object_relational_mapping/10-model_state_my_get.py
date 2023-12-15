aa9f9ed9bff1a14e7e54411dd1b99f735266f32a
Mkellia <k.muzira@gmail.com>
2023-12-15 04:43:34 -0800

    break begind

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-"""list all states with name equal to the fourth argument"""
+"""
+Writes out the State object with the name passed as argument from the db
+"""
+
+
+import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sys import argv
+
 
 if __name__ == '__main__':
-    user = argv[1]
-    password = argv[2]
-    db_name = argv[3] 
-    db = 'mysql+mysqldb://{}:{}@localhost/{}'.format(user, password, db_name)
-    engine = create_engine(db, pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State)\
-        .filter_by(name=state_name).order_by(State.id).first()
-    if state:
-        print(str(state.id))
-    else:
-        print('Not found')
+
+    state = session.query(State).filter(State.name == sys.argv[4]).first()
+    print("Not found" if not state else state.id)
