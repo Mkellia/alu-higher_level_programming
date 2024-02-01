40c7e654fbebcdae2e64194960c1a3d26719ebf5
Mkellia <k.muzira@gmail.com>
2024-02-01 03:47:27 -0800

    Arguments

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const numArgs = process.Argv.length;
+if (numArgs === 2){
+	console.log('No argument');}
+else if (numArgs === 3){
+	console.log('Argument found');}
+else {
+	console.log('Arguments found');}
