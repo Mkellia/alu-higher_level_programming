2285459bc5db9e33f1ebab6c8a2a90a0355b392a
root <k.muzira@alustudent.com>
2024-02-01 23:39:10 -0800

    javascript

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,9 +1,3 @@
 #!/usr/bin/node
-
-const args = process.argv.slice(2);
-
-const arg1 = args[0] || 'undefined';
-const arg2 = args[1] || 'undefined';
-
-console.log(arg1 + ' is ' + arg2);
-
+const string = process.argv[2] + ' is ' + process.argv[3];
+console.log(string);
